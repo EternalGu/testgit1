de874c177e5e91be838ca5e052b6541c1e190fc8
Zver <liashchenkomisha@gmail.com>
2023-09-23 17:46:08 +0300

    neww2

--- test.py
+++ test.py
@@ -10,16 +10,15 @@ class MyTest(unittest.TestCase):
         self.assertEqual(adder(a=10, b=11), 21)
 
     def test_mixed(self):
-        self.assertEqual(adder(2, a=4), 6)
+        self.assertEqual(adder(2, b=4), 6)
 
     def test_dif(self):
         x = 10
         y = 0
-        self.assertEqual(adder(0, -5, y, a=x), 5)
+        self.assertEqual(adder(0, 5, y, a=x), 5)
 
     def test_wrong(self):
         self.assertEqual(adder('2', 'abc', 10), 12)
-
 
 
 
