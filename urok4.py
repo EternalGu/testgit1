5398fbae50816f762acd75f57d89a517c42da4fd
EternalGu <liashchenkomisha@gmail.com>
2023-09-30 17:50:28 +0300

    nn commit

--- urok4.py
+++ urok4.py
@@ -1,3 +1,12 @@
 import sqlite3
 
 con = sqlite3.connect("itstep_db.sl3")
+
+cur = con.cursor()
+#cur.execute("CREATE TABLE IF NOT EXISTS students (name TEXT)# ")
+#cur.execute("INSERT INTO students (name) values ('Vanya')")
+cur. execute("SELECT rowid, name FROM students")
+print(cur.fetchall())
+con.commit()
+
+con.close()
