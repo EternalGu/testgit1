b13eb7ef9abf12b2dbe034647a22d82dc84e9110
Zver <liashchenkomisha@gmail.com>
2023-10-07 14:10:16 +0300

    neww2

--- dzNew.py
+++ dzNew.py
@@ -1,44 +1,40 @@
-class Tiger:
-    def __init__(self, name, classes, type):
+class Animal:
+    def __init__(self, name, type, age):
         self.name = name
-        self.classes = classes
         self.type = type
+        self.age = age
+#__str__ посомтрел в интренете так как выдавало ошибку искал как исправить
+    def __str__(self):
+        return f"{self.name} {self.type} {self.age}"
 
-    def show_info(self):
-        print(f"Name: {self.name}, class: {self.classes}, type: {self.type}")
-
-
-class Crocodile:
-    def __init__(self, name, classes, type):
-        self.name = name
-        self.classes = classes
-        self.type = type
-
-    def show_info(self):
-        print(f"Name: {self.name}, class: {self.classes}, type: {self.type}")
-
-
-class Kangaroo:
-    def __init__(self, name, classes, type):
-        self.name = name
-        self.classes = classes
-        self.type = type
-
-    def show_info(self):
-        print(f"Name: {self.name}, class: {self.classes}, type: {self.type}")
-
-
-tiger1 = Tiger("mani", "predator", "Tiger")
-tiger1.show_info()
-
-
-crocodile1 = Crocodile("", "predator", "Crocodile")
-crocodile1.show_info()
-
-
-kangaroo1 = Kangaroo("agata", "herbivorous", "Kangaroo")
-kangaroo1.show_info()
 
 class Zoo:
     def __init__(self):
+        self.animals = []
+
+    def add_animal(self, animal):
+        self.animals.append(animal)
+        print(f"{animal.name} join the world")
+
+    def show_animals(self):
+        print("Animals in the zoo:")
+        for animal in self.animals:
+            print(animal)
+
+
+tiger = Animal("rudy", "Lion", 5)
+crocodile = Animal("kamp", "corocodile", 10)
+kangaroo = Animal("agata", "kangaroo", 7)
+
+zoo = Zoo()
+zoo.add_animal(tiger)
+zoo.add_animal(crocodile)
+zoo.add_animal(kangaroo)
+
+zoo.show_animals()
+
+
+
+
+
 
